3bbb7282e80f67c96a7df8b4c147726865f1f46e
Artemma07 <101255236+Artemma07@users.noreply.github.com>
2022-05-11 01:39:28 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,25 +1,36 @@
 #ifndef HEADER_H
 #define HEADER_H
 #include <iostream>
-#include <time.h>
-using namespace std;
-void bubbleSort(int arr1[], unsigned n);
 
-void bubbleSort(float arr[], unsigned n);
+class Complex {
+private:
+	double real;
+	double imag;
+public:
 
-void print(int arr1[], unsigned n);
+	Complex();
+	Complex(double r, double i);
 
-void print(float arr[], unsigned n);
+	double module();
+	void printComplex();
 
-void random_numbers(int arr1[], unsigned n);
+	void set_imag(double im);
+	double get_imag();
 
-void random_numbers(float arr[], unsigned n);
+	void set_real(double re);
+	double get_real();
+};
 
-void delete_memory(float* arr);
-
-void delete_memory(int* arr1);
-
-
+void printIntArray(int* mas, int n);
+void printDoubleArray(double* mas, int n);
+void printComplexArray(Complex* mas, int n);
 
 
+int* createArray(int* mas, int n);
+double* createArray(double* mas, int n);
+Complex* createArray(Complex* mas, int n);
+
+int* sortBubble(int* mas, int n);
+double* sortBubble(double* mas, int n);
+Complex* sortBubble(Complex* mas, int n);
 #endif
