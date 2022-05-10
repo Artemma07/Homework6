d907ad010ce8902c614b889cb5b0049b7b84079f
Artemma07 <101255236+Artemma07@users.noreply.github.com>
2022-05-11 01:40:11 +0300

    Update Header.cpp

--- Header.cpp
+++ Header.cpp
@@ -1,63 +1,147 @@
 #include "Header.h"
-void bubbleSort(int arr1[], unsigned n)
-{
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            if (arr1[j] > arr1[j + 1]) {
-                int b = arr1[j];
-                arr1[j] = arr1[j + 1];
-                arr1[j + 1] = b;
-            }
-        }
-    }
-}
-void bubbleSort(float arr[], unsigned n)
-{
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int b = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = b;
-            }
-        }
-    }
-}
-void print(int arr1[], unsigned n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr1[i] << " ";
-    }
+Complex::Complex() {
+	real = 0;
+	imag = 0;
 }
 
-void print(float arr[], unsigned n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+Complex::Complex(double r, double i) {
+	real = r;
+	imag = i;
 }
-void random_numbers(int arr1[], unsigned n)
-{
-    srand((unsigned int)time(0));
-    for (int i = 0; i < n; i++)
-    {
-        arr1[i] = rand() % 100 - 50;
 
-    }
+double Complex::module() {
+	return sqrt(this->imag * this->imag + this->real * this->real);
 }
-void random_numbers(float arr[], unsigned n)
-{
-    srand((unsigned int)time(0));
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = (rand() % 100 - 50) * 0.5;
-    }
+
+
+void Complex::printComplex() {
+	if (imag > 0) {
+		std::cout << real << '+' << imag << 'i' << std::endl;
+	}
+	else if (imag < 0) {
+		std::cout << real << '-' << abs(imag) << 'i' << std::endl;
+	}
+	else {
+		std::cout << real << std::endl;
+	}
 }
-void delete_memory(float* arr) {
-    delete[]arr;
+
+void Complex::set_imag(double im) {
+	imag = im;
 }
-void delete_memory(int* arr1) {
-    delete[]arr1;
+
+double Complex::get_imag() {
+	return imag;
+}
+
+void Complex::set_real(double re) {
+	real = re;
+}
+
+double Complex::get_real() {
+	return real;
+}
+
+
+
+int* createArray(int* mas, int n) {
+	mas = new int[n];
+	for (int i = 0; i < n; i++) {
+		mas[i] = rand() % 100;
+	}
+	return mas;
+}
+
+double* createArray(double* mas, int n) {
+	mas = new double[n];
+	for (int i = 0; i < n; i++) {
+		mas[i] = rand() % 100;
+	}
+	return mas;
+}
+
+Complex* createArray(Complex* mas, int n) {
+	mas = new Complex[n];
+	for (int i = 0; i < n; i++) {
+		mas[i].set_imag(rand() % 100);
+		mas[i].set_real(rand() % 100);
+
+	}
+	return mas;
+}
+
+void printIntArray(int* mas, int n) {
+	for (int i = 0; i < n; i++) {
+		std::cout << mas[i] << " ";
+	}
+	std::cout << "\n";
+}
+
+void printDoubleArray(double* mas, int n) {
+	for (int i = 0; i < n; i++) {
+		std::cout << mas[i] << " ";
+	}
+	std::cout << "\n";
+}
+
+void printComplexArray(Complex* mas, int n) {
+	for (int i = 0; i < n; i++) {
+		mas[i].printComplex();
+	}
+	std::cout << "\n";
+}
+
+
+int* sortBubble(int* mas, int n) {
+	int* buf = new int [n];
+	for (int i = 0; i < n; i++) {
+		buf[i] = mas[i];
+	}
+	int buffer;
+	for (int i = 0; i < n-1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (buf[j] > buf[j + 1]) {
+				buffer = buf[j];
+				buf[j] = buf[j + 1];
+				buf[j + 1] = buffer;
+			}
+		}
+	}
+	return buf;
+}
+
+double* sortBubble(double* mas, int n) {
+	double* buf = new double[n];
+	for (int i = 0; i < n; i++) {
+		buf[i] = mas[i];
+	}
+	double buffer;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (buf[j] > buf[j + 1]) {
+				buffer = buf[j];
+				buf[j] = buf[j + 1];
+				buf[j + 1] = buffer;
+			}
+		}
+	}
+	return buf;
+}
+
+Complex* sortBubble(Complex* mas, int n) {
+	Complex* buf = new Complex[n];
+	for (int i = 0; i < n; i++) {
+		buf[i] = mas[i];
+	}
+	Complex buffer;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (buf[j].module() > buf[j + 1].module()) {
+				buffer = buf[j];
+				buf[j] = buf[j + 1];
+				buf[j + 1] = buffer;
+			}
+		}
+	}
+	return buf;
 }
